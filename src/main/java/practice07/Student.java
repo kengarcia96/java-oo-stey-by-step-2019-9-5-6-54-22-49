3a16d073ad2cbbe933d14bd3ebe759d068b779ee
John Kenneth Garcia <kengarcia.96@gmail.com>
2019-09-24 06:14:23 +0800

    Answers for Practice 7

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,23 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        return (super.introduce() + " I am a Student. I am at " + klass.getDisplayName() + ".");
+    }
+
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,41 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        if (getKlass() == null) {
+            return (super.introduce() + " I am a Teacher. I teach No Class.");
+        }
+        else{
+            return (super.introduce() + " I am a Teacher. I teach " + klass.getDisplayName() + ".");
+        }
+
+    }
+
+    public String introduceWith(Student student) {
+
+        if (getKlass().equals(student.getKlass())) {
+            return (super.introduce() + " I am a Teacher. I teach " + student.getName() + ".");
+        }
+        else{
+            return (super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".");
+        }
+
+    }
 }
